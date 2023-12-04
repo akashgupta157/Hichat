ab1ce84ede24e3d013fc6ba3a4af2ca8efac19dc
Akash Gupta <akashgupta989322@gmail.com>
2023-12-04 17:13:08 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,39 @@
 import React from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import Chat from "./Pages/Chat";
 import Menu from "./Components/Menu";
 import Setting from "./Pages/Setting";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
+import { useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 export default function App() {
   const { pathname } = useLocation();
+  const auth = useSelector((store) => store.auth.isAuthenticated);
+  function PrivateRoute({ children }) {
+    return auth ? children : <Navigate to="/" />;
+  }
   return (
     <div className="flex">
       {(pathname === "/chats" || pathname === "/setting") && <Menu />}
       <Routes>
-        <Route path="/chats" element={<Chat />} />
-        <Route path="/setting" element={<Setting />} />
+        <Route
+          path="/chats"
+          element={
+            <PrivateRoute>
+              <Chat />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/setting"
+          element={
+            <PrivateRoute>
+              <Setting />
+            </PrivateRoute>
+          }
+        />
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
