8d19fb3213aaea218109f2cce4e297b6f07cb756
Akash Gupta <akashgupta989322@gmail.com>
2023-12-09 14:26:47 +0530

    update

--- client/src/Components/misc.js
+++ client/src/Components/misc.js
@@ -1,5 +1,5 @@
-// export const url = "https://hichat-backend.vercel.app";
-export const url = "http://localhost:3000";
+export const url = "https://hichat-backend.vercel.app";
+// export const url = "http://localhost:3000";
 export function configure(token) {
   return {
     headers: {
@@ -10,23 +10,17 @@ export function configure(token) {
 export function formatTime(timestamp) {
   const date = new Date(timestamp);
   const currentDate = new Date();
-
-  // Check if the date is the current date
   const isCurrentDate = date.toDateString() === currentDate.toDateString();
-
   if (isCurrentDate) {
-    // If it's the current date, show only time
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const period = hours >= 12 ? "PM" : "AM";
     const formattedHours = hours % 12 || 12;
     return `${formattedHours}:${minutes < 10 ? "0" : ""}${minutes} ${period}`;
   } else {
-    // If it's not the current date, show date and time
     const day = date.getDate();
-    const month = date.getMonth() + 1; // Months are zero-based
-    const year = date.getFullYear() % 100; // Get last two digits of the year
-
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear() % 100;
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const period = hours >= 12 ? "PM" : "AM";
